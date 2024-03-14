cb7258e9bb5e5e50054980d4df09acdd03e75215
Sohalt <paolo@nextjournal.com>
2024-03-14 16:31:11 +0100

    Add docstrings (#259)

--- src/nextjournal/garden_clerk.clj
+++ src/nextjournal/garden_clerk.clj
@@ -3,19 +3,25 @@
             [nextjournal.clerk :as clerk]
             [nextjournal.simple-http-server :as http]))
 
-(defn assoc-some [m k v]
+(defn- assoc-some [m k v]
   (if (some? v)
     (assoc m k v)
     m))
 
-(defn serve! [opts]
+(defn serve!
+  "Serve dynamic clerk notebooks on application.garden.
+  Takes the same options as `nextjournal.clerk/serve!`"
+  [opts]
   (when-let [persistent-storage (System/getenv "GARDEN_STORAGE")]
     (System/setProperty "clerk.cache_dir" (str (fs/path persistent-storage ".clerk" "cache")) ))
   (clerk/serve! (-> opts
                     (assoc :bind "0.0.0.0")
                     (assoc-some :port (System/getenv "GARDEN_PORT")))))
 
-(defn serve-static! [opts]
+(defn serve-static!
+  "Build clerk notebooks and serve the resulting html on application.garden.
+  Takes the same options as `nextjournal.clerk/build!`"
+  [opts]
   (def out-path (str (fs/create-temp-dir)))
   (when-let [persistent-storage (System/getenv "GARDEN_STORAGE")]
     (System/setProperty "clerk.cache_dir" (str (fs/path persistent-storage ".clerk" "cache")) ))
